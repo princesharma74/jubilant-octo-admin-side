e6b0172bafead82204f2060923ca73f411136d48
Prince Sharma <princesharma2899@gmail.com>
2024-04-18 16:16:21 +0530

    initial commit

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,88 @@
+"use client"
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "react-hot-toast";
+
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { Modal } from "@/components/ui/modal"
+import { 
+        Form, 
+        FormControl, 
+        FormField, 
+        FormItem, 
+        FormLabel, 
+        FormMessage 
+    } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import axios from "axios";
+
+const formSchema = z.object({
+    name: z.string().min(1)
+})
+
+export const StoreModal = () => {
+    const storeModal = useStoreModal(); 
+
+    const [loading, setloading] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try{
+            setloading(true);
+            const response = await axios.post('/api/stores', values);
+            // after the store is created. Make sure it loads store dashboard
+            window.location.assign(`/${response.data.id}`); // why did we use routes? Because, it makes sure that the store is ready before loading.
+        }
+        catch(error){
+            toast.error("Something went wrong")
+        }
+        finally{
+            setloading(false);
+        }
+    }
+
+    return (
+        <Modal
+            title="Create Store"
+            description="Add a new store to manage products and cotegories"
+            isOpen={storeModal.isOpen}
+            onClose={storeModal.onClose}
+        >
+            <div className="space-y-4 py-2 pb-4">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+                        <FormField
+                            control={form.control}
+                            name = "name"
+                            render={({ field })=>(
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input disabled = {loading} placeholder="E-Commerce" {...field}/>
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )}
+                        />
+                        <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                            <Button disabled = {loading} variant="outline" onClick={storeModal.onClose}>
+                                Cancel
+                            </Button>
+                            <Button disabled = {loading} type="submit">Continue</Button>
+                        </div>
+                    </form>
+                </Form>
+            </div>
+        </Modal>
+    )
+}
