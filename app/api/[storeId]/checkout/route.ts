b3374cf2cd13f70afd6ab56a1a4c72ad519f7748
Prince Sharma <princesharma2899@gmail.com>
2024-05-20 09:39:43 +0530

    Payment issue fixed

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -38,11 +38,11 @@ export async function POST(
         line_items.push({
             quantity: 1, 
             price_data: {
-                currency: 'USD', 
+                currency: 'INR', 
                 product_data: {
                     name: product.name, 
                 }, 
-                unit_amount: product.price.toNumber()
+                unit_amount: ( product.price.toNumber()*100 )
             }
         })
     })
